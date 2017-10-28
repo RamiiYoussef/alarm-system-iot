7de012fc1ec58dd80dcafd4f79bb9f80fd7db036
Rami Mo7ep <ramy.moh3p@gmail.com>
2017-10-28 07:23:57 +0200

    Upload Script

--- core/config.py
+++ core/config.py
@@ -2,12 +2,12 @@
 #Created By Rami Mohep
 
 #Replace with your actual Email Server credentials
-FROM = 'rai.raspberry20@gmail.com'
-TO   = 'ramy.moh3p@gmail.com'
-PASS = 'blackraspberry'
+FROM = ''
+TO   = ''
+PASS = ''
 
 #Replace with your actual Database credentials
 server = 'localhost'
 database = 'door_interactive'
-username = 'rami'
-password = 'volvo'
+username = ''
+password = ''
